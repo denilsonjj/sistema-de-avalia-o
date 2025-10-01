eeac26f26633dd5068259d193d915fdd7b8fd4ad
Denilson junior <94057171+denilsonjj@users.noreply.github.com>
2025-10-01 21:32:21 +0000

    chore: Desativa scheduler e rotas de teste para deploy

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+// backend/index.js
+
 require('dotenv').config();
 
 const express = require('express');
@@ -10,8 +12,8 @@ const selfAssessmentRoutes = require('./routes/selfAssessmentRoutes');
 const feedbackRoutes = require('./routes/feedbackRoutes');
 const goalRoutes = require('./routes/goalRoutes');
 const reportRoutes = require('./routes/reportRoutes');
-const testRoutes = require('./routes/testRoutes');
-const { startScheduler } = require('./services/schedulerService')
+// const testRoutes = require('./routes/testRoutes'); // <--- COMENTE ESTA LINHA
+// const { startScheduler } = require('./services/schedulerService') // <--- COMENTE ESTA LINHA
 const oeeRoutes = require('./routes/oeeRoutes');
 const productionLineRoutes = require('./routes/productionLineRoutes')
 const app = express();
@@ -27,11 +29,11 @@ app.use('/api/self-assessment', selfAssessmentRoutes);
 app.use('/api/feedbacks', feedbackRoutes);
 app.use('/api/goals', goalRoutes);
 app.use('/api/reports', reportRoutes);
-app.use('/api/test', testRoutes);
+// app.use('/api/test', testRoutes); // <--- COMENTE ESTA LINHA
 app.use('/api/oee', oeeRoutes);
 app.use('/api/production-lines', productionLineRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
-  startScheduler(); // Inicia o agendador de tarefas
+  // startScheduler(); // <--- COMENTE ESTA LINHA
 });
